d9f3136a431619117622d89c2dbaaf18105bf026
Sam28YO <sam28jayant@gmail.com>
2024-07-08 03:01:33 +0530

    signup

--- front/notes/src/App.jsx
+++ front/notes/src/App.jsx
@@ -1,20 +1,23 @@
 import React from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import Home from "./pages/home/home";
 import Login from "./pages/login/login";
 import SignUp from "./pages/signup/signup";
-const routes = (
-  <BrowserRouter>
-    <Routes>
-      <Route path="/dashboard" exact element={<Home />} />
-      <Route path="/login" exact element={<Login />} />
-      <Route path="/signup" exact element={<SignUp />} />
-    </Routes>
-  </BrowserRouter>
-);
 
 function App() {
-  return <div>{routes}</div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        {/* Redirect from root to /signup */}
+        <Route path="/" element={<Navigate to="/signup" />} />
+
+        {/* Define your routes */}
+        <Route path="/dashboard" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<SignUp />} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
